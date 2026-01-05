4fc5011be59a031e040cf687ae1bc11633de8605
Narayanam Vamsi <vamikrish.rock@gmail.com>
2026-01-05 17:12:01 +0530

    first commit

--- script.js
+++ script.js
@@ -3,26 +3,57 @@ function addTask() {
   let input = document.getElementById("task");
   let task = input.value;
 
-  if (task === ""){ 
-    return
-  };
+  if (task === "") {
+    return;
+  }
 
   let li = document.createElement("li");
   let span = document.createElement("span");
 
   span.innerText = task;
 
-  span.onclick = function () {
-  span.classList.toggle("done");
-  };
+
+  span.addEventListener("click", function () {
+    span.classList.toggle("done");
+  });
 
   let btn = document.createElement("button");
-
   btn.innerText = "x";
 
-  btn.onclick = function () {
+  btn.addEventListener("click", function () {
     li.remove();
-  };
+  });
+
+  li.appendChild(span);
+  li.appendChild(btn);
+
+  document.getElementById("list").appendChild(li);
+  input.value = "";
+}
+function addTask() {
+
+  let input = document.getElementById("task");
+  let task = input.value;
+
+  if (task === "") {
+    return;
+  }
+
+  let li = document.createElement("li");
+  let span = document.createElement("span");
+
+  span.innerText = task;
+
+  span.addEventListener("click", function () {
+    span.classList.toggle("done");
+  });
+
+  let btn = document.createElement("button");
+  btn.innerText = "x";
+
+  btn.addEventListener("click", function () {
+    li.remove();
+  });
 
   li.appendChild(span);
   li.appendChild(btn);
