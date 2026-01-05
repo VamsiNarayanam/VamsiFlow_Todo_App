a025e6a662b6d9dc601ac5e2c495c15389517fb3
Narayanam Vamsi <vamikrish.rock@gmail.com>
2026-01-05 16:16:40 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+function addTask() {
+
+  let input = document.getElementById("task");
+  let task = input.value;
+
+  if (task === ""){ 
+    return
+  };
+
+  let li = document.createElement("li");
+  let span = document.createElement("span");
+
+  span.innerText = task;
+
+  span.onclick = function () {
+  span.classList.toggle("done");
+  };
+
+  let btn = document.createElement("button");
+
+  btn.innerText = "x";
+
+  btn.onclick = function () {
+    li.remove();
+  };
+
+  li.appendChild(span);
+  li.appendChild(btn);
+
+  document.getElementById("list").appendChild(li);
+  input.value = "";
+}
