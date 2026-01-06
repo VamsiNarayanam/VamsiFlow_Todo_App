bf4d53e64992a18eb03179d40d878783529b1b4b
Narayanam Vamsi <vamikrish.rock@gmail.com>
2026-01-06 14:07:48 +0530

    showcase info if task is not entered

--- script.js
+++ script.js
@@ -1,33 +1,34 @@
 function addTask() {
-
   let input = document.getElementById("task");
-  let task = input.value;
+  let task = input.value.trim(); 
+  let error = document.getElementById("error");
 
   if (task === "") {
+    error.innerText = "Please enter a task";
     return;
   }
+
+  error.innerText = "";
 
   let li = document.createElement("li");
   let span = document.createElement("span");
 
-  span.addEventListener("click", () => {
-    span.classList.add("done");
-   });
-
   span.innerText = task;
+
+  span.addEventListener("click", () => {
+    span.classList.add("done"); 
+  });
 
   let btn = document.createElement("button");
   btn.innerText = "X";
 
-  btn.addEventListener("click", function () {
+  btn.addEventListener("click", () => {
     li.remove();
   });
 
   li.appendChild(span);
   li.appendChild(btn);
 
-  let ui = document.getElementById("list")
-  ui.appendChild(li);
+  document.getElementById("list").appendChild(li);
   input.value = "";
 }
-
